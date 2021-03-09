9a1d213ded07fb288b62a76be1b686f7b64f5526
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-03-09 19:30:53 +0000

    SignUp form Completed

--- register.php
+++ register.php
@@ -92,47 +92,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> -->
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <div class="wrapper">
+<div class="container">
+<?php
+    include('includes/header.php');
+    ?>
+    <div id="loginForm">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <!-- UserName -->
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>" placeholder="Username">
+                <input type="text" name="username" value="<?php echo $username; ?>" placeholder="Username">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>   
              <!--Password-->
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>"placeholder="Password">
+                <input type="password" name="password" value="<?php echo $password; ?>"placeholder="Password">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <!-- Confirm Password -->
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>"placeholder="Confirm Password">
+                <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>"placeholder="Confirm Password">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
+                <input type="submit" value="Submit">
+                <input type="reset"  value="Reset">
             </div>
             <p>Already have an account? <a href="login.php">Login</a>.</p>
         </form>
-    </div>    
-</body>
-</html>
+    </div>  
+</div>  
 <?php
 include('includes/footer.php');
 ?>
