58f1598cb2f1a809bdebb8a4b92716d826ddc34a
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-03-24 13:58:43 +0000

    Validation link finxed in header

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <title>Jersey Direct</title>
 <link rel="stylesheet" type="text/css" href="css/mystyle.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script language="JavaScript" src="gen_validatorv31.js" type="text/javascript"></script>
+<script language="JavaScript" src="../gen_validatorv31.js" type="text/javascript"></script>
 </head>
 <!-- the body section -->
 <body>
