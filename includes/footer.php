31d7da9526e31b6d6313912432f796b255026dee
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-03-24 12:35:08 +0000

    Contact.php created and html moved to php

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,13 @@
 <p>&copy; <?php echo date("Y"); ?> Jersey Direct App</p>
 </footer>
 </div><!-- close div container -->
+<script language="JavaScript">
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name"); 
+frmvalidator.addValidation("email","req","Please provide your email"); 
+frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+</script>
+
 </body>
 <script src="validation.js"></script>
 </html>
