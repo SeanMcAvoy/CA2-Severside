b8e1df7dd7f2e729f7a0c178bb76fe28b06d8fe3
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-04-14 11:32:39 +0100

    Only admin can now edit and delete products

--- manage-products.php
+++ manage-products.php
@@ -1,7 +1,11 @@
 <?php
 // Initialize the session
 session_start();
-
+if(($_SESSION["username"]) != "mcavoy1129")
+{
+    header("location: index.php");
+    exit;
+}
 //Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
